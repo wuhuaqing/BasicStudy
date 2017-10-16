cb5fb240e9767c4fbe9349ec9ac3d1c6ea6d8314
whq <2339857040@qq.com>
2017-10-16 16:27:14 +0800

    d

---
+++ src/MainTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by admin on 2017/10/16.
+ */
+public class MainTest {
+   public static String str3 = " ";
+   public static void main(String[] args){
+        //String []  strs = str.split("~",-1);
+        String []  strs = str3.split("~",-1);
+        System.out.println(strs);
+
+    }
+}
