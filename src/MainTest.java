94e3323c2e0c98bc3d9a71c0f9ae8e2350fba10c
whq <2339857040@qq.com>
2017-11-10 17:13:30 +0800

    thread test

--- src/MainTest.java
+++ src/MainTest.java
@@ -2,11 +2,42 @@
  * Created by admin on 2017/10/16.
  */
 public class MainTest {
-   public static String str3 = " ";
-   public static void main(String[] args){
-        //String []  strs = str.split("~",-1);
-        String []  strs = str3.split("~",-1);
-        System.out.println(strs);
+    public int value = 1;
+
+    public static void main(String[] args){
+        final  MainTest mainTest = new MainTest(); 
+          new Thread(new Runnable() {
+             @Override
+             public void run() {
+                 try {
+                     mainTest.a();
+                 } catch ( Exception e) {//InterruptedException
+                     e.printStackTrace();
+                 }
+             }
+         }).start();
+
+         new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mainTest.a();
+                    System.out.println();
+                   // Thread.sleep(3000);
+                } catch ( Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
 
     }
+
+    public  void a(){
+        System.out.println("...method a init...."+System.currentTimeMillis());
+    }
+
+    public void b(){
+        System.out.println("method a init....");
+    }
+
 }
